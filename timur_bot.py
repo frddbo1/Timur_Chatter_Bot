ff87c9a8385a7fd49cf4b6c5ff311aff428440af
frddbo1 <w.tsaryov123@gmail.com>
2026-05-20 18:47:47 +0300

    Update timur_bot.py

--- timur_bot.py
+++ timur_bot.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 # ================= КОНФИГУРАЦИЯ =================
 TELEGRAM_TOKEN = "8455818639:AAEvMCnXthyxT-UMMvzwd1WRKAa3BMqdkQ0"
 GROQ_API_KEY = "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"
-OPENROUTER_API_KEY = "sk-or-v1-ТВОЙ_РАБОЧИЙ_КЛЮЧ_ОТ_OPENROUTER_СЮДА"
+OPENROUTER_API_KEY = "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"
 
 PRIMARY_MODEL = "llama-3.3-70b-versatile"        
 FALLBACK_MODEL = "google/gemini-2.5-flash:free"  
