efb0769f9202876a50b34cc8173c4455cd42760a
frddbo1 <w.tsaryov123@gmail.com>
2026-05-22 03:40:07 +0300

    Update timur_bot.py

--- timur_bot.py
+++ timur_bot.py
@@ -21,8 +21,8 @@ GROQ_API_KEY = "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"
 OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
 # Самые стабильные и неубиваемые модели на OpenRouter
-PRIMARY_MODEL = "mistral-nemo"  # Это для Groq (жрёт очень мало токенов лимита)
-FALLBACK_MODEL = "meta-llama/llama-3-8b-instruct:free"
+PRIMARY_MODEL = "llama-3.1-8b-instant"  # Это для Groq (жрёт очень мало токенов лимита)
+FALLBACK_MODEL = "openai/gpt-oss-20b"
 # ================================================
 
 SYSTEM_PROMPT = (
